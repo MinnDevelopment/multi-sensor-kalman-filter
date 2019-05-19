98d4bfff782bcd91a9b0ee85dff7d79bf8549803
Florian Spieß <business@minnced.club>
2019-05-19 13:10:24 +0200

    Add positional sensor

--- planes/__init__.py
+++ planes/__init__.py
@@ -1,13 +1,12 @@
-from numpy import sin, cos, pi, sqrt
-from numpy.random import standard_normal
-from time import time
-import matplotlib.animation as animation
-import matplotlib.pyplot as plt
+from threading import Timer
+
 import numpy as np
+from numpy import sin, cos, pi, sqrt
+from numpy.random import standard_normal, normal
 
 
 class GroundTruth:
-    def __init__(self, v, q, rounds=1):
+    def __init__(self, v=300, q=9, rounds=1):
         self.v = v
         self.q = q
         self.w = q / 2*v
@@ -25,104 +24,71 @@ class GroundTruth:
         return self.a * np.array([[ self._x(t) ],
                                   [ self._y(t) ]])
 
-    def acceleration(self, t):
-        return self.v * np.array([[ cos(self.w * t) / 2 ],
-                                  [ cos(2 * self.w * t) ]])
-
     def velocity(self, t):
         return -self.q * np.array([[ sin(self.w * t) / 4 ],
                                    [ sin(2 * self.w * t) ]])
 
+    def acceleration(self, t):
+        return self.v * np.array([[ cos(self.w * t) / 2 ],
+                                  [ cos(2 * self.w * t) ]])
 
-class Sensor:
-    def __init__(self, error=50, interval=5):
+    def x(self, t):
+        return np.vstack([self.trajectory(t), self.velocity(t), self.acceleration(t)])
+
+
+class GenericSensor:
+    def __init__(self, sigma=50, interval=5):
         self.truth = GroundTruth(300, 9)
-        self.sigma = error
+        self.sigma = sigma
         self.interval = interval
         self.real_point = None
+        self._traj_timer = None
 
-        self._next_read = 0
-        self._point = None
-        self._accel = None
-        self._veloc = None
-        self._ax = None
-        self._circle = None
-        self._radius = self.sigma * 3
+    def __schedule(self, fn):
+        timer = Timer(self.interval, fn)
+        timer.start()
+        return timer
+
+    def reset(self):
+        if self._traj_timer:
+            self._traj_timer.cancel()
+
+    def on_trajectory(self, callback, at):
+        def runner():
+            callback(self.get_trajectory_data(at()))
+        if self._traj_timer:
+            self._traj_timer.cancel()
+        self._traj_timer = self.__schedule(runner)
 
     def get_error(self):
-        vec = standard_normal((2, 1))
-        print("Normal: ", vec.flatten())
-        return self.sigma * vec
+        return self.sigma * standard_normal((2, 1))
 
     def get_trajectory_data(self, t):
         return self.truth.trajectory(t) + self.get_error()
 
-    def get_acceleration_data(self, t):
-        return self.truth.acceleration(t) # + self.sigma * normal(size=(2, 1))
 
-    def get_velocity_data(self, t):
-        return self.truth.velocity(t) # + self.sigma * normal(size=(2, 1))
+class PositionalSensor:
+    def __init__(self, pos, sigma_range=20, sigma_azimuth=0.2):
+        self.truth = GroundTruth()
+        self.pos = pos
+        self.sigma_range = sigma_range
+        self.sigma_azimuth = sigma_azimuth
 
-    def plot_frame(self, t):
-        real_position = self.truth.trajectory(t)
-        self.real_point.set_data(real_position)
-        if self._next_read > time():
-            return self._point
-        else:
-            return self.plot_trajectory(t, real_position)
+    def get_error(self):
+        return np.array([self.sigma_range * normal(),
+                         self.sigma_azimuth * normal()]).T
 
-    def plot_init(self, ax):
-        self._ax = ax
-        self._point, = ax.plot([0], [0], "ro")
-        self.real_point, = ax.plot([0], [0], "go")
-        # plot track function
-        data = [self.truth.trajectory(t) for t in self.truth.space]
-        t, d = ([p[0] for p in data], [p[1] for p in data])
-        ax.plot(t, d, 'grey', alpha=0.5)
+    def get_range_azimuth(self, t):
+        (x_real, y_real) = self.truth.trajectory(t).flatten()
+        (x_sensor, y_sensor) = self.pos
+        new_x = sqrt((x_real - x_sensor)**2 + (y_real - y_sensor)**2)
+        new_y = np.arctan((y_real - y_sensor) / (x_real - x_sensor))
+        return np.array([new_x, new_y]).T
 
-        self._accel = ax.arrow(0, 0, 0, 0, width=100, color="r")
-        self._veloc = ax.arrow(0, 0, 0, 0, width=100, color="b")
-
-    def plot_trajectory(self, t, real_pos):
-        self._next_read = time() + self.interval
-        error = self.get_error()
-        pos = error + real_pos
-        print("Error: ", error.flatten())
-        self._point.set_data(pos)
-        if self._circle:
-            self._circle.remove()
-        self._circle = plt.Circle(pos, self._radius, color="r", fill=False)
-        self._ax.add_artist(self._circle)
-        self.plot_arrows(t, pos)
-        return self._point
-
-    def plot_arrows(self, t, pos):
-        a = (10 * self.get_acceleration_data(t)).flatten().tolist()
-        b = (200 * self.get_velocity_data(t)).flatten().tolist()
-        self._accel.remove()
-        self._veloc.remove()
-        (x, y) = pos.flatten()
-        self._accel = self._ax.arrow(x, y, *a, width=100, color="r")
-        self._veloc = self._ax.arrow(x, y, *b, width=100, color="b")
-
-
-class KalmanFilter:
-    def __init__(self, sensor, sigma):
-        self.sensor = sensor
-        self.sigma = sigma
-        self._r = self.sigma**2 * np.eye(2)
 
 
 def main():
-    sensor = Sensor()
-    frames = sensor.truth.space
-
-    fig, ax = plt.subplots()
-    ax.set_ylim(-11000, 11000)
-    ax.set_xlim(-11000, 11000)
-    sensor.plot_init(ax)
-    ani = animation.FuncAnimation(fig, sensor.plot_frame, frames, repeat=True, interval=10)
-    plt.show()
+    print(PositionalSensor((0, 0)).get_range_azimuth(100))
 
 
 if __name__ == '__main__':
